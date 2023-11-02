72770aa4b4e5cc11db224688102bf28b843159a2
lchidlovsky <l.chidlovsky@outlook.fr>
2023-11-02 18:44:01 +0100

    utilisation de la manette possible

--- AirFight.py
+++ AirFight.py
@@ -6,42 +6,94 @@ sys.path.insert(0, "./src/")
 from src import *
 
 
-
-def gestion_controls(event):
+def gestion_controls():
     global jeu_lance
+    global vaisseau
+    global utilisation_clavier
+    global manette
+    global conf_bouttons
     global dep_haut, dep_bas, dep_gauche, dep_droit
     
-    if event.type == pygame.QUIT:
-            jeu_lance = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                jeu_lance = False
         
-    if event.type == pygame.KEYDOWN:
-        if event.key == K_UP:
-            dep_haut = True
-        if event.key == K_DOWN:
-            dep_bas = True
-        if event.key == K_LEFT:
-            dep_gauche = True
-        if event.key == K_RIGHT:
-            dep_droit = True
+        if event.type == pygame.JOYDEVICEADDED:
+            pygame.joystick.init()
+            manette = pygame.joystick.Joystick(event.device_index)
+            utilisation_clavier = False
+            dep_haut, dep_bas, dep_gauche, dep_droit = False, False, False, False
+            print("\nPassage en mode manette !\n")
             
-    if event.type == pygame.KEYUP:
-        if event.key == K_UP:
-            dep_haut = False
-        if event.key == K_DOWN:
-            dep_bas = False
-        if event.key == K_LEFT:
-            dep_gauche = False
-        if event.key == K_RIGHT:
-            dep_droit = False
+        if event.type == pygame.JOYDEVICEREMOVED:
+            utilisation_clavier = True
+            dep_haut, dep_bas, dep_gauche, dep_droit = False, False, False, False
+            print("\nPassage en mode clavier !\n")
+        
+        if utilisation_clavier:     #si le joueur utilise le clavier
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_UP:
+                    dep_haut = True
+                if event.key == K_DOWN:
+                    dep_bas = True
+                if event.key == K_LEFT:
+                    dep_gauche = True
+                if event.key == K_RIGHT:
+                    dep_droit = True
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == K_UP:
+                    dep_haut = False
+                if event.key == K_DOWN:
+                    dep_bas = False
+                if event.key == K_LEFT:
+                    dep_gauche = False
+                if event.key == K_RIGHT:
+                    dep_droit = False
+                    
+        else:       #si le joueur utilise la manette
+
+            for nom, num in conf_bouttons.items():
+                if manette.get_button(num):
+                    print("Bouton", nom, "pressé !")
+                    
+                    if nom == 'A':
+                        print("Aaaaaaaaahh !!")
+                    
+            #gestion du joystick gauche de la manette
+            if manette.get_axis(1) < - 0.2:
+                dep_haut = True
+            else:
+                dep_haut = False
+            if manette.get_axis(1) > 0.2:
+                dep_bas = True
+            else:
+                dep_bas = False
+            if manette.get_axis(0) < - 0.2:
+                dep_gauche = True
+            else:
+                dep_gauche = False
+            if manette.get_axis(0) > 0.2:
+                dep_droit = True
+            else:
+                dep_droit = False
+                         
+    if dep_haut: vaisseau.haut(vitesse_joueur)
+    if dep_bas: vaisseau.bas(vitesse_joueur)
+    if dep_gauche: vaisseau.gauche(vitesse_joueur)
+    if dep_droit: vaisseau.droite(vitesse_joueur)
+
 
 
 #mise en place de la fenêtre de jeu
 pygame.init()
 screen = pygame.display.set_mode()
-SCREEN_HEIGHT, SCREEN_WIDTH = screen.get_size()
-SCREEN_HEIGHT = int(SCREEN_HEIGHT * 0.8)
-SCREEN_WIDTH = int(SCREEN_WIDTH * 0.8)
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+SCREEN_HEIGHT = int(screen.get_height() * 0.8)
+SCREEN_WIDTH = int(screen.get_width() * 0.8)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+print("height =", SCREEN_HEIGHT)
+print("width =", SCREEN_WIDTH)
 pygame.display.set_caption(nom_du_jeu)
 
 clock = pygame.time.Clock()
@@ -51,7 +103,13 @@ clock = pygame.time.Clock()
 apparences_vaisseau = []
 for i in range(1, 7):
     apparences_vaisseau.append(pygame.image.load(f"images/joueur/joueur_{i}.png").convert_alpha())
-vaisseau = Joueur(apparences_vaisseau, 200, 10, SCREEN_HEIGHT, SCREEN_WIDTH)
+vaisseau = Joueur(apparences_vaisseau, 200, 10, SCREEN_WIDTH, SCREEN_HEIGHT)
+print("len", len(apparences_vaisseau))
+
+#contrôles du joueurs
+utilisation_clavier = True
+manette = None
+conf_bouttons = conf_ps
 
 #variable de déplacement du vaisseau
 dep_haut, dep_bas, dep_gauche, dep_droit = False, False, False, False
@@ -60,16 +118,12 @@ jeu_lance = True
 while jeu_lance:
     clock.tick(FPS)
     
-    for event in pygame.event.get():
-        gestion_controls(event)
-            
-    if dep_haut: vaisseau.haut(vitesse_joueur)
-    if dep_bas: vaisseau.bas(vitesse_joueur)
-    if dep_gauche: vaisseau.gauche(vitesse_joueur)
-    if dep_droit: vaisseau.droite(vitesse_joueur)
+    gestion_controls()
     
     
     screen.fill((200, 200, 200))
+    
+    vaisseau.tick()
     
     screen.blit(vaisseau.visuel(), vaisseau.coordonnees())
 
--- src/constantes.py
+++ src/constantes.py
@@ -1,3 +1,35 @@
 nom_du_jeu = "Air Fight"
+
 FPS = 60
+
 vitesse_joueur = 5
+
+zone_morte = 0.2
+
+conf_xbox = {'A': 0,
+             'B': 1,
+             'X': 2,
+             'Y': 3,
+             'VIEW': 6,
+             'MENU': 7,
+             'L': 8,
+             'R': 9,
+             'LB': 4,
+             'RB': 5}
+
+conf_ps = {'A': 0,
+           'B': 1,
+           'X': 2,
+           'Y': 3,
+           'VIEW': 4,
+           'MENU': 6,
+           'L': 7,
+           'R': 8,
+           'UP': 11,
+           'LB': 9,
+           'RB': 10,
+           'DOWN': 12,
+           'LEFT': 13,
+           'RIGHT': 14,
+           'PAD': 15,
+           'BRAND': 5}
